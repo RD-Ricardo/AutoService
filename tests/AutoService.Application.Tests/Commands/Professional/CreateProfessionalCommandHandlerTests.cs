e1d19a2d918eec6c8e4e7b84350b984cbcaf6f4d
RD-Ricardo <ricardodias169@gmail.com>
2024-03-05 23:56:54 -0300

    chore: teste em processo de desenvolvimento

---
+++ tests/AutoService.Application.Tests/Commands/Professional/CreateProfessionalCommandHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using AutoService.Application.Commands.Professional.CreateProfessional;
+using AutoService.Core.Web;
+using AutoService.Domain.Interfaces;
+using MediatR;
+using Moq;
+using Moq.AutoMock;
+
+namespace AutoService.Application.Tests.Commands.Professional
+{
+    public class CreateProfessionalCommandHandlerTests
+    {
+
+        [Fact(DisplayName = "Adicionar professional")]
+        [Trait("Categoria", "Professional - Create")]
+        public async Task CreateProfessional_New_Sucecs()
+        {
+            //Arrage
+            var command = new CreateProfessionalCommand("Ricardo", "teste", "teste", "dsads", "Ricardo");
+            var mocker = new AutoMocker();
+            var commandHandler = mocker.CreateInstance<CreateProfessionalCommandHandler>();
+
+            //Act
+            var result = await commandHandler.Handle(command, CancellationToken.None);
+
+            //Assert
+            var resultValid = new RequestResult<Unit>(success: true);
+            Assert.Equal(resultValid, result);
+            mocker.GetMock<IProfessionalRepository>().Verify(r => r.CreateAsync(It.IsAny<Domain.Entities.Professional>()), Times.Once);
+        }
+    }
+}
---
+++ tests/AutoService.Infrastructure.Tests/Communciation/CustomerServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using AutoService.Core.Web.Extensions;
+using AutoService.Infrastructure.Communication.CustomerService;
+using AutoService.Payment.Infrastructure.Communication.DTOs;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace AutoService.Infrastructure.Tests.Communciation
+{
+    public class CustomerServiceTests
+    {
+        [Fact]
+        [Trait("Categoria", "Customer communication service succes")]
+        public async Task CustomerCommunicationService_Create_Succes()
+        {
+            // Arrage
+            var customerDTO = new CreateCustomerDTO() { };
+            var paymentConfig = new Mock<IOptions<PaymentSettings>>();
+            var customerCommunicationService = new CustomerCommunicationService(paymentConfig.Object);
+
+            //Act 
+            var result = await customerCommunicationService.CreateCustomer(customerDTO);
+
+            //Assert
+            Assert.NotNull(result);
+        }
+    }
+}
