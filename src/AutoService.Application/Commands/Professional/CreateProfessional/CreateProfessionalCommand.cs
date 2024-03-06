1cc43cac8d5ccf08922363297e2ad23bb4f40224
RD-Ricardo <ricardodias169@gmail.com>
2024-03-05 23:50:23 -0300

    fix: alterando construtor

--- src/AutoService.Application/Commands/Professional/CreateProfessional/CreateProfessionalCommand.cs
+++ src/AutoService.Application/Commands/Professional/CreateProfessional/CreateProfessionalCommand.cs
@@ -4,17 +4,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AutoService.Application.Commands.Professional.CreateProfessional
 {
-    public class CreateProfessionalCommand : Command<Unit>
+    public class CreateProfessionalCommand(string email, string password, string passwordConfirmed, string cPF, string name) : Command<Unit>
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string PasswordConfirmed { get; set; }
-        public string CPF { get; set; }
-        public string Name { get; set; }
+        public string Email { get; private set; } = email;
+        public string Password { get; private set; } = password;
+        public string PasswordConfirmed { get; private set; } = passwordConfirmed;
+        public string CPF { get; private set; } = cPF;
+        public string Name { get; private set; } = name;
 
         public override bool Validate()
         {
-           return ValidationResult.IsValid;
+           return true;
         }
     }
 }
