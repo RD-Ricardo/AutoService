776b5276b63726b7f72b9167d9bb5af54ec3019f
RD-Ricardo <ricardodias169@gmail.com>
2024-03-07 22:00:29 -0300

    feat: tabela de log de auditoria

---
+++ src/AutoService.Domain/Entities/Audit.cs
@@ -0,0 +1,14 @@
+﻿using AutoService.Core.DomainObjects;
+
+namespace AutoService.Domain.Entities
+{
+    public class Audit : AggregateRoot
+    {
+        public string System { get; set; }
+        public Guid UserId { get; set; }
+        public string Browser { get; set; }
+        public string Action { get; set; }
+        public string Description { get; set; }
+        public bool Mobile { get; set; }
+    }
+}
