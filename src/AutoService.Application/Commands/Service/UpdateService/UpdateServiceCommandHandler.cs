52a3bf0b19e3f2aee62f9e0d5628f6585767fb11
RD-Ricardo <ricardodias169@gmail.com>
2024-03-07 22:27:46 -0300

    feat: comando de update service

--- src/AutoService.Application/Commands/Service/UpdateService/UpdateServiceCommandHandler.cs
+++ src/AutoService.Application/Commands/Service/UpdateService/UpdateServiceCommandHandler.cs
@@ -1,14 +1,35 @@
-﻿using AutoService.Core.Abstractions.CQRS.Handlers;
+﻿using AutoService.Application.Extensions;
+using AutoService.Core.Abstractions.CQRS.Handlers;
+using AutoService.Core.Data;
 using AutoService.Core.Web;
+using AutoService.Domain.Interfaces;
 using MediatR;
 
 namespace AutoService.Application.Commands.Service.UpdateService
 {
     public class UpdateServiceCommandHandler : CommandHandler<UpdateServiceCommand, Unit>
     {
-        public override Task<RequestResult<Unit>> Handle(UpdateServiceCommand message, CancellationToken cancellationToken)
+        private readonly IServiceRepository _repository;
+        public UpdateServiceCommandHandler(IServiceRepository repository)
         {
-            throw new NotImplementedException();
+            _repository = repository;
+        }
+
+        public override async Task<RequestResult<Unit>> Handle(UpdateServiceCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.Validate())
+                return Fail(message.ValidationResult.ReturnErros());
+
+            Domain.Entities.Service service = new(message.Name,
+                message.Description,
+            message.Value,
+                message.ProfessionalId);
+
+            _repository.UpdateServiceAsync(service);
+
+            await _repository.UnitOfWork.Commit();
+
+            return Success();
         }
     }
 }
